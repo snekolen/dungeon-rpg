5d587d54c2944abb1101ba0524e0af1b07481469
Helen (Ma) Cantao <h.cantao1216@gmail.com>
2026-08-14 18:02:26 -0500

    Initial commit

---
+++ Characters/Player/IdleState.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class IdleState : PlayerState
+{
+
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GD.Print("Idle state");
+
+        if(characterNode.direction != Vector2.Zero)
+        {
+            characterNode.StateMachineNode.SwitchState<MoveState>();
+        }
+    }
+    
+
+    public override void _Input(InputEvent @event)
+    {
+        if (Input.IsActionJustPressed(GameConstants.INPUT_DASH))
+        {
+            characterNode.StateMachineNode.SwitchState<DashState>();
+        }
+    }
+
+
+    protected override void EnterState()
+    {
+        base.EnterState();
+
+        characterNode.AnimPlayerNode.Play(GameConstants.ANIM_IDLE);
+    }
+
+}
---
+++ General/GameConstants.cs
@@ -0,0 +1,18 @@
+public class GameConstants
+{
+    //Animations
+    public const string ANIM_IDLE = "idle";
+    public const string ANIM_MOVE = "move";
+    public const string ANIM_DASH = "dash";
+
+    //Inputs
+    public const string INPUT_MOVE_LEFT = "move_left";
+    public const string INPUT_MOVE_RIGHT = "move_right";
+    public const string INPUT_MOVE_FORWARD = "move_forward";
+    public const string INPUT_MOVE_BACKWARD = "move_backward";
+    public const string INPUT_DASH = "dash";
+
+    // Notification
+    public const int NOTIFICATION_ENTER_STATE = 5001;
+    public const int NOTIFICATION_EXIT_STATE = 5002;
+}
