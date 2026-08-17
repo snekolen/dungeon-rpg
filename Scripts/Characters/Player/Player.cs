c291540ba404056b7b1f277ceda45d66a8e73a98
Helen (Ma) Cantao <h.cantao1216@gmail.com>
2026-08-16 20:46:00 -0500

    Squashed commit of the following:
    commit 332d4d629864eb6642fddc204b15c49d638ff43b
    Author: Helen (Ma) Cantao <h.cantao1216@gmail.com>
    Date:   Sun Aug 16 20:45:08 2026 -0500
    
        Completed enemy
    
    commit 9bd036ef0eb1bea21a0d4a4ab8d31e8bf4071b5f
    Author: Helen (Ma) Cantao <h.cantao1216@gmail.com>
    Date:   Sun Aug 16 15:54:07 2026 -0500
    
        Added enemy

---
+++ Scripts/Characters/Player/Player.cs
@@ -0,0 +1,10 @@
+using Godot;
+using System;
+
+public partial class Player : Character
+{
+    public override void _Input(InputEvent @event)
+    {
+        direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_BACKWARD);
+    }
+}
